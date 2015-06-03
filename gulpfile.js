bc8353e08eceb0bd9f4325e43840a6ca0b3aaef8
abram arias <aarias@Abrahams-MacBook-Pro.local>
2015-06-03 01:50:29 -0700

    fixing the last commit

--- gulpfile.js
+++ gulpfile.js
@@ -20,12 +20,157 @@ gulp.task('Replace', function () {
            return '<%';
           }
         },
+
           {
             match: /\}/g,
             replacement: function(){
             return '%>';
              }
+          },
+          ,
+
+          {
+            match: /\<meta if\=\"\!string\.IsNullOrEmpty\(/g,
+            replacement: function(){
+            return '<%if (Data.';
+             }
+          },
+          ,
+
+          {
+            match: /\<div if\=\"/g,
+            replacement: function(){
+            return '<%if (Data.';
+             }
+          },
+
+          {
+            match: /\<if condition\=\"/g,
+            replacement: function(){
+            return '<%if (Data.';
+             }
+          },
+
+          {
+            match: /\<else\>/g,
+            replacement: function(){
+            return '<% } else { %>';
+             }
+          },
+
+          {
+            match: /\#Html\.RenderAction\(\"Css\"\, \"Srs\"\, new \{ domainCulture \= Model\.DomainCulture \}\)\;/g,
+            replacement: function(){
+            return '<%- Data.Css %>';
+             }
           }
+          ,
+
+          {
+            match: /\#Html\.RenderAction\(\"Favicon\"\, \"Srs\"\, new \{ domainCulture \= Model\.DomainCulture \}\)\;/g,
+            replacement: function(){
+            return '<%- Data.Favicon %>';
+             }
+          }
+          ,
+
+          {
+            match: /\#Html\.RenderAction\(\"JsHead\"\, \"Srs\"\, new \{ domainCulture \= Model\.DomainCulture \}\)\;/g,
+            replacement: function(){
+            return '<%- Data.JsHead %>';
+             }
+          }
+          ,
+
+          {
+            match: /\#Html.RenderAction\(\"Js\"\, \"Srs\"\, new \{ domainCulture \= Model\.DomainCulture\, jqueryOnBody \= false \}\)\;/g,
+            replacement: function(){
+            return '<%- Data.Js %>';
+             }
+          },
+
+          {
+            match: /\<render partial\=\"/g,
+            replacement: function(){
+            return '<% include  ../';
+             }
+          },
+          {
+            match: /\<render partial\=\"Redesign\/Filters\/\_Filters\" filters\=\"Model\" \/\>/g,
+            replacement: function(){
+            return '<% include  ../Redesign/Filters/_Filters %>';
+             }
+          },
+           {
+            match: /\<render partial\=\"Redesign\/Results\/\_ResultsTable" resultsTable\=\"Model\" \/\>/g,
+            replacement: function(){
+            return '<% include  ../Redesign/Results/_ResultsTable  %>';
+             }
+          },
+           {
+            match: /\<render partial\=\"Redesign\/Results\/\_NoResults\" noResults\=\"Model\" clearLastFilter\=\"false\" \/\>/g,
+            replacement: function(){
+            return '<% include  ../Redesign/Results/_NoResults  %>';
+             }
+          },
+
+          {
+            match: /\#Html\.RenderAction\(\"Header\"\, \"Srs\"\, new \{ pageType \= Model\.Display\.SrsPageType\, domainCulture \= Model\.DomainCulture\, metro \= Model\.Display\.Metro \}\)\;/g,
+            replacement: function(){
+            return '<%- Data.Header %>';
+             }
+          },
+          ,
+
+          {
+            match: /\<render partial\=\"\_FilterLocationGroup" filterLocationGroup\=\"filters\" \/\>/g,
+            replacement: function(){
+            return '<% include  ../Redesign/filters/_FilterLocationGroup  %>';
+             }
+          },
+          {
+            match: /\<render partial\=\"\_FilterCuisine\" filterCuisine\=\"filters\.Filters\.Cuisines\" selectedCount\=\"filters\.Filters\.SelectedCuisineCount\" \/\>/g,
+            replacement: function(){
+            return '<% include  ../Redesign/filters/_FilterCuisine  %>';
+             }
+          },
+          {
+            match: /\<render partial\=\"\_FilterOffers\" filterOffers\=\"filters\" filterGroupId\=\"\'offer\_type\_filters\'\" \/\>/g,
+            replacement: function(){
+            return '<% include  ../Redesign/filters/_FilterOffers  %>';
+             }
+          },
+          {
+            match: /\<render partial\=\"\_FilterPrice\" filterPrice\=\"filters\.Filters\.PriceBands\" selectedCount\=\"filters\.Filters\.SelectedPriceBandCount\" \/\>/g,
+            replacement: function(){
+            return '<% include  ../Redesign/filters/_FilterPrice  %>';
+             }
+          },
+          {
+            match: /\<render partial\=\"\_FilterExactTime\" filterExactTime\=\"filters\" \/\>/g,
+            replacement: function(){
+            return '<% include  ../Redesign/filters/_FilterExactTime  %>';
+             }
+          },
+          {
+            match: /\<render partial\=\"\_FilterPopOnly\" filterPopOnly\=\"filters\" \/\>/g,
+            replacement: function(){
+            return '<% include  ../Redesign/filters/_FilterPopOnly  %>';
+             }
+          },
+           {
+            match: /\<render partial\=\"\_FilterViewOptions\" filterViewOptions\=\"filters\" \/\>/g,
+            replacement: function(){
+            return '<% include  ../Redesign/filters/_FilterViewOptions  %>';
+             }
+          },
+           {
+            match: /\<render partial\=\"\_FilterSortOrder\" request\=\"filters\.Request\" \/\>/g,
+            replacement: function(){
+            return '<% include  ../Redesign/filters/_FilterSortOrder  %>';
+             }
+          },
+
       ]
     }))
     .pipe(gulp.dest('Templates-spark'));
