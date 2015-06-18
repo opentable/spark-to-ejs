66d4dd03cf47c15ee1f947a0df8ecc2ca50b926e
a-arias <aarias@nearsoft.com>
2015-06-18 12:16:32 -0700

    Adding the foreachRule and also refactor set_rules.js

--- src/rules_engine.js
+++ src/rules_engine.js
@@ -1,27 +1,11 @@
 'use strict';
 var setRules = require("./set_rules.js");
-
-exports.rulesEngine = function (data, cheerio) {
-    var $ = cheerio.load(data, {
-            recognizeSelfClosing : true
-        }),
-        selector = null,
-        elements = null,
-        rule = null,
-        output = null;
-
-    //0 output = ""
-    //1. Get all nodes from beginning & loop
-    //1.1  find a matching rule for the current node
-    //1.2  snipet = execute rule
-    //1.3  output +=snipet
-    //2. save new ejs file (output)
-
+    
     function traverseDom(element) {
-        var content = "", structure;
+        var content = "", 
             structure = setRules.applyRule(element),
             output = "";
-
+        console.log(element.name,element.type);
         if(element.children !== undefined) {
             for(var i=0; i<element.children.length; i++ ) {
                 content += "\n\t" + traverseDom(element.children[i]);
@@ -35,5 +19,9 @@ exports.rulesEngine = function (data, cheerio) {
         return output;
     }
 
+exports.rulesEngine = function (data, cheerio) {
+    var $ = cheerio.load(data, {
+            recognizeSelfClosing : true
+        });
     return traverseDom($.root()[0]);
 };
--- src/set_rules.js
+++ src/set_rules.js
@@ -20,25 +20,38 @@
  *
  */
 function ifRule(element) {
-    var ifTag = "<% if(:condition){%>",
-        ifEnd = "<%}%>",
-        condition,
+    var startTag = "<% if(:condition){%>",
+        endTag = "<%}%>",
+        condition = element.attribs['condition'];
 
-    condition = element.attribs['condition'];
     if(condition !== undefined) {
-        ifTag = ifTag.replace(':condition',condition);
+        startTag =startTag.replace(':condition',condition);
     }
-    return [ifTag, ifEnd]
+    return [startTag, endTag];
+}
+function foreachRule(element) {
+    
+    var startTag = " <% :object.forEach(function(:index) { %>",
+          endTag = "<%)}%>",
+          argumentsFor  = element.attribs['each'].split(" ");
+   
+    if(arguments.length>0) {
+        startTag =startTag.replace(':object',argumentsFor[3]).replace(':index',argumentsFor[1]);
+    }
+    return [startTag, endTag];
 }
 
 exports.applyRule = function(element) {
-    if(element.name === "if") {
-        return  ifRule(element);
-    }
-
-    if(element.name) {
-        return ["<"+element.name+">", "<"+ element.name + "/>"];
-    }
-    return ["",""];
-
+   switch(element.name) {
+        case "if":
+            return  ifRule(element);
+        case "for":
+            return  foreachRule(element);
+        case "render":
+        default:
+            if(element.name) {
+                 return ["<"+element.name+">", "<"+ element.name + "/>"];   
+            }
+            return ["", ""];      
+   }
 };
