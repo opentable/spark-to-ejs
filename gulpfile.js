d4b09a4db791be8c7d7d74a1ce7bfc55453b57bb
a-arias <aarias@nearsoft.com>
2015-06-22 20:28:47 -0700

    We've added the renderIf rule and We did some refactor to the code

--- gulpfile.js
+++ gulpfile.js
@@ -7,20 +7,20 @@ var gulp        = require('gulp'),
     engine      = require('./lib/rulesEngine.js'),
     cheerio     = require('cheerio'),
     jshint      = require('gulp-jshint');
- 
+
 
 gulp.task('Lint', function () {
    return gulp.src('./lib/*.js')
        .pipe(jshint())
        .pipe(jshint.reporter('default'));
 });
- 
+
 gulp.task('Change_Extensions',  function() {
     return gulp.src('./Views/**/*.spark')
       .pipe(ext_replace('.ejs'))
       .pipe(gulp.dest('./templates-ejs'))
 });
- 
+
 gulp.task("Read_File", function() {
     return gulp.src('./templates-ejs/**/*.ejs')
         .pipe(through.obj(function (file, enc,cb) {
@@ -31,7 +31,7 @@ gulp.task("Read_File", function() {
             });
         }))
 });
- 
+
 gulp.task('build', function(callback) {
   runSequence('Lint','Change_Extensions','Read_File',callback);
 });
