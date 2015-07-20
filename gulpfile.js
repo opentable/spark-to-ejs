d59e7b305bdfe824394738f098b9a5a60df642c6
Matt Blair <matt@mattblair.co>
2015-07-20 16:24:31 -0700

    fixed issue where target directory wasnt cleaned and files were getting parsed again

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,68 @@
-var config = require('./config');
-var gulp = require('gulp'),
-  ext_replace = require('gulp-ext-replace'),
-  replace = require('gulp-replace-task'),
-  runSequence = require('run-sequence'),
-  fs = require("fs"),
-  through = require('through2'),
-  engine = require('./lib/rulesEngine.js'),
-  cheerio = require('cheerio'),
-  jshint = require('gulp-jshint'),
-  path = require("path"),
-  Source = "",
-  c = 0;
+var cheerio = require('cheerio');
+var del = require('del');
+var fs = require('fs');
+var path = require('path');
+var runSequence = require('run-sequence');
+var through = require('through2');
 
-gulp.task('Lint', function () {
+var gulp = require('gulp');
+var ext_replace = require('gulp-ext-replace');
+var replace = require('gulp-replace-task');
+var jshint = require('gulp-jshint');
+
+var config = require('./config');
+var engine = require('./lib/rulesEngine.js');
+
+var source = "";
+var c = 0;
+
+gulp.task('lint', function () {
   return gulp.src('./lib/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
 
+gulp.task('clean', function (cb) {
+  del([config.outPath + '/**/*.ejs'], { force: true }, cb);
+});
+
 /*this task is going to read all the spark templates and change its extension to .EJS*/
-gulp.task('Change_Extensions', function () {
+gulp.task('changeExtensions', function () {
   return gulp.src(config.inputPath + '/**/*.spark')
     .pipe(ext_replace('.ejs'))
     .pipe(gulp.dest(config.outPath))
 });
 
 /*Read_File is going to read all the templates and apply the conversion rules and save some information for the report.html.*/
-gulp.task("Read_File", function () {
+gulp.task("readFile", function () {
   return gulp.src(config.outPath + '/**/*.ejs')
     .pipe(through.obj(function (file, enc, cb) {
       c += 1;
-      Source += "<li><strong>Converted: </strong> <a href=" + file.path + " target='_blank'>" + path.basename(file.path) + "</a></li></br>\n";
+      source += "<li><strong>Converted: </strong> <a href=" + file.path + " target='_blank'>" + path.basename(file.path) + "</a></li></br>\n";
 
       process.stdout.write(file.path + '\n');
 
-      var content = String(file.contents),
-        output = engine.rulesEngine(content, cheerio);
+      var content = file.contents.toString();
+      var output = engine.rulesEngine(content, cheerio);
+
       fs.writeFile(file.path, output, function (err) {
-        cb(null, file);
+        if(err) {
+          process.stdout.write('ERR: ' + err.toString() + ' \n');
+          cb(err);
+        } else {
+          cb(null, file);
+        }
+
       });
     }))
 });
 
 /*this task is going to write a report of the converting process*/
-gulp.task("Write_Report", function () {
+gulp.task("writeReport", function () {
   return gulp.src('./Reports/layout/LayoutReport.html')
     .pipe(through.obj(function (file, enc, cb) {
       var content = String(file.contents);
-      var output = Source;
+      var output = source;
       var d = new Date();
       var date = d.toLocaleDateString();
       fs.writeFile('./Reports/report' + d + '.html', content + output + "</ol>\
@@ -62,5 +77,5 @@ gulp.task("Write_Report", function () {
 });
 
 gulp.task('build', function (callback) {
-  runSequence('Lint', 'Change_Extensions', 'Read_File', "Write_Report", callback);
+  runSequence('lint', 'clean', 'changeExtensions', 'readFile', "writeReport", callback);
 });
