63e97b774c144226ebca8c0ed09731922e0be513
Matt Blair <matt@mattblair.co>
2015-08-11 18:24:47 -0700

    add better <viewdata/> support

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 require('babel/register');
 
 module.exports = {
-   inputPath : '../SearchResults/Web/Views/',
-   outPath  :  '../search-result-node/app/views/ejs-templates'
+   inputPath: '../SearchResults/Web/Views/',
+   outPath: '../search-result-node/app/views/ejs-templates'
 };
